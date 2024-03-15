b0dbd5193091c9628f02a999aac8864f9d05c85f
muzcodes <153247335+muzcodes@users.noreply.github.com>
2024-03-15 11:18:42 +0800

    changes to app.py, some updates regarding to the plots

--- app.py
+++ app.py
@@ -22,73 +22,73 @@ st.write(df_distribution)
 fig_pie = px.pie(df_distribution, names='Business Name', values='counts', title='Distribution of Franchises')
 st.plotly_chart(fig_pie)
 
-# Number of restaurants in each state:
-fig_bar = px.bar(df.groupby('state').size().reset_index(name='State counts'), x='state', y='State counts', title='Number of Restaurants in Each State')
-st.plotly_chart(fig_bar)
+# # Number of restaurants in each state:
+# fig_bar = px.bar(df.groupby('state').size().reset_index(name='State counts'), x='state', y='State counts', title='Number of Restaurants in Each State')
+# st.plotly_chart(fig_bar)
 
-# Number of restaurants in each city:
-fig_bar = px.bar(df.groupby('city').size().reset_index(name='City counts'), x='city', y='City counts', title='Number of Restaurants in Each City')
-st.plotly_chart(fig_bar)
+# # Number of restaurants in each city:
+# fig_bar = px.bar(df.groupby('city').size().reset_index(name='City counts'), x='city', y='City counts', title='Number of Restaurants in Each City')
+# st.plotly_chart(fig_bar)
 
-# Sort users in descending order by rating or number of fans:
-options = ['Average Stars', 'Number Of Fans']
-selected_option = st.selectbox('Select sorting option:', options)
+# # Sort users in descending order by rating or number of fans:
+# options = ['Average Stars', 'Number Of Fans']
+# selected_option = st.selectbox('Select sorting option:', options)
 
-if selected_option == 'Average Stars':
-    fig_bar = px.bar(df.sort_values('user_average_stars', ascending=False), x='user_name', y='user_average_stars', title='Top Users by Average Stars')
-    st.plotly_chart(fig_bar)
-elif selected_option == 'Number Of Fans':
-    fig_bar = px.bar(df.sort_values('user_fans', ascending=False), x='user_name', y='user_fans', title='Top Users by Number of Fans')
-    st.plotly_chart(fig_bar)
-else:
-    st.write('Please select at least one option.')
+# if selected_option == 'Average Stars':
+#     fig_bar = px.bar(df.sort_values('user_average_stars', ascending=False), x='user_name', y='user_average_stars', title='Top Users by Average Stars')
+#     st.plotly_chart(fig_bar)
+# elif selected_option == 'Number Of Fans':
+#     fig_bar = px.bar(df.sort_values('user_fans', ascending=False), x='user_name', y='user_fans', title='Top Users by Number of Fans')
+#     st.plotly_chart(fig_bar)
+# else:
+#     st.write('Please select at least one option.')
 
-# Top rating restaurants in selected state or US:
-selected_states = st.multiselect('Select states:', df['state'].unique())
+# # Top rating restaurants in selected state or US:
+# selected_states = st.multiselect('Select states:', df['state'].unique())
 
-if selected_states:
-    filtered_df = df[df['state'].isin(selected_states)]
-    fig_bar = px.bar(filtered_df.sort_values('stars', ascending=False), x='name', y='stars', title='Top-Rated Restaurants in Selected States')
-    st.plotly_chart(fig_bar)
-else:
-    fig_bar = px.bar(df.sort_values('stars', ascending=False), x='name', y='stars', title='Top-Rated Restaurants in the US')
-    st.plotly_chart(fig_bar)
+# if selected_states:
+#     filtered_df = df[df['state'].isin(selected_states)]
+#     fig_bar = px.bar(filtered_df.sort_values('stars', ascending=False), x='name', y='stars', title='Top-Rated Restaurants in Selected States')
+#     st.plotly_chart(fig_bar)
+# else:
+#     fig_bar = px.bar(df.sort_values('stars', ascending=False), x='name', y='stars', title='Top-Rated Restaurants in the US')
+#     st.plotly_chart(fig_bar)
 
-# Top rating restaurants by selected cuisine:
-selected_cuisines = st.multiselect('Select cuisines:', df['category'].unique())
+# # Top rating restaurants by selected cuisine:
+# selected_cuisines = st.multiselect('Select cuisines:', df['category'].unique())
 
-if selected_cuisines:
-    filtered_df = df[df['category'].isin(selected_cuisines)]
-    fig_bar = px.bar(filtered_df.sort_values('stars', ascending=False), x='name', y='stars', title='Top-Rated Restaurants by Selected Cuisines')
-    st.plotly_chart(fig_bar)
-else:
-    st.write('Please select at least one cuisine.')
+# if selected_cuisines:
+#     filtered_df = df[df['category'].isin(selected_cuisines)]
+#     fig_bar = px.bar(filtered_df.sort_values('stars', ascending=False), x='name', y='stars', title='Top-Rated Restaurants by Selected Cuisines')
+#     st.plotly_chart(fig_bar)
+# else:
+#     st.write('Please select at least one cuisine.')
 
-# Latest review of selected restaurant:
-selected_id = st.text_input('Enter the business ID:')
+# # Latest review of selected restaurant:
+# selected_id = st.text_input('Enter the business ID:')
 
-if selected_id:
-    if selected_id in df['rev_business_id'].values:
-        latest_review = df[df['rev_business_id'] == selected_id].sort_values('rev_date', ascending=False).iloc[0]
-        st.write(f"Latest review for business ID {selected_id}:")
-        st.write(f"User ID: {latest_review['rev_user_id']}")
-        st.write(f"Stars: {latest_review['rev_stars']}")
-        st.write(f"Date: {latest_review['rev_date']}")
-        st.write(f"Review text: {latest_review['rev_text']}")
-        st.write(f"Useful: {latest_review['rev_useful']}, Funny: {latest_review['rev_funny']}, Cool: {latest_review['rev_cool']}")
-    else:
-        st.write('Invalid business ID')
-else:
-    st.write('Please enter a business ID.')
+# if selected_id:
+#     if selected_id in df['rev_business_id'].values:
+#         latest_review = df[df['rev_business_id'] == selected_id].sort_values('rev_date', ascending=False).iloc[0]
+#         st.write(f"Latest review for business ID {selected_id}:")
+#         st.write(f"User ID: {latest_review['rev_user_id']}")
+#         st.write(f"Stars: {latest_review['rev_stars']}")
+#         st.write(f"Date: {latest_review['rev_date']}")
+#         st.write(f"Review text: {latest_review['rev_text']}")
+#         st.write(f"Useful: {latest_review['rev_useful']}, Funny: {latest_review['rev_funny']}, Cool: {latest_review['rev_cool']}")
+#     else:
+#         st.write('Invalid business ID')
+# else:
+#     st.write('Please enter a business ID.')
 
-# Elite vs regular
-elite_count = df[df['user_elite'] != ''].shape[0]
-regular_count = df[df['user_elite'] == ''].shape[0]
+# # Elite vs regular
+# elite_count = df[df['user_elite'] != ''].shape[0]
+# regular_count = df[df['user_elite'] == ''].shape[0]
 
-df_counts = pd.DataFrame({'User_Type': ['Elite Users', 'Regular Users'], 'Count': [elite_count, regular_count]})
+# df_counts = pd.DataFrame({'User_Type': ['Elite Users', 'Regular Users'], 'Count': [elite_count, regular_count]})
 
-fig_pie = px.pie(df_counts, values='Count', names='User_Type', title='Elite Users vs Regular Users')
-st.plotly_chart(fig_pie)
+# fig_pie = px.pie(df_counts, values='Count', names='User_Type', title='Elite Users vs Regular Users')
+# st.plotly_chart(fig_pie)
 
 # Run the Streamlit app
 if __name__ == "__main__":
